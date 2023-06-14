0cd40fa2b6a85ee82b1c005a1e2023ed7bb4765c
Mac <mac@MacBook-Air-Mac.local>
2023-06-14 10:15:04 +0500

    b10

--- basic_func10.py
+++ basic_func10.py
@@ -1,9 +1,8 @@
 # Create a function called main.
-def main():
+def main(a):
 
-# Create function arguments a.
-    a=5 
+# Create function arguments a. 
 
 # Return the opposite value of a.
     return a*(-1)
-print(main())
+print(main(8))
